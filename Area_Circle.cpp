2f7c8d3d40d783e1a2b89c47396dd656078ade45
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-06-07 16:13:40 +0530

    Create Area_Circle.cpp

---
+++ Area_Circle.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+     float r;
+     float area;
+     
+     cout<<"Enter the radius of Circle :";
+     cin>>r;
+     
+     area=3.1425*r*r;
+     
+     cout<<"Area of a Circle is :"<<area<<endl;
+ 
+}
