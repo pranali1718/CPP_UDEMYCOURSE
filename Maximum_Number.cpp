3893e94df648d3b8b1d2c155fec70f1f06ade2c2
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-06-07 16:13:13 +0530

    Create Maximum_Number.cpp

---
+++ Maximum_Number.cpp
@@ -0,0 +1,16 @@
+int main()
+{
+    int num1 ,num2;
+    cout<<"Enter two numbers:";
+    cin>>num1>>num2;
+    if(num1>num2)
+    {
+        cout<<"greater no is:"<<num1;
+    }
+    else
+    {
+        cout<<"greater no is :"<<num2;
+    }
+    return 0;
+ 
+}
