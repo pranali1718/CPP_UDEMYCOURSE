9a3c5a4486bbedf8eaeff167db937583bd88c26d
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-06-07 16:12:27 +0530

    Create Positive_Negative.cpp

---
+++ Positive_Negative.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout<<"Enter Number:";
+    cin>>num;
+    if(num>=0)
+    {
+        cout<<"Positive";
+    }
+    else
+    {
+        cout<<"Negative";
+    }
+    return 0;
+ 
+}
