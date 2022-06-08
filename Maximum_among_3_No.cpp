92b6f8f4e1808c43d5903da65d662e7c2a05538b
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-06-08 10:51:22 +0530

    Create Maximum_among_3_No.cpp

---
+++ Maximum_among_3_No.cpp
@@ -0,0 +1,27 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout<<"Enter Numbers :";
+    cin>>a>>b>>c;
+    if(a>b && a>c)
+    {
+        cout<<" greater no is: "<<a;
+    }
+    else
+    {
+        if(b>c)
+        {
+            cout<<"greater no is: "<<b;
+        }
+        else
+        {
+            cout<<"greater no is: "<<c;
+        }
+    }
+}
