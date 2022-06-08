2b6a593013e7e4767b39657873400365dec6fdcd
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-06-08 17:39:02 +0530

    Create Day_count_no.cpp

---
+++ Day_count_no.cpp
@@ -0,0 +1,43 @@
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the Number :";
+    cin>>n;
+    if(n==1)
+    {
+        cout<<"Monday";
+    }
+    else if(n==2)
+    {
+        cout<<"Tuesday";
+    }
+    else if(n==3)
+    {
+        cout<<"Wed";
+    }
+    else if(n==4)
+    {
+        cout<<"Thu";
+    }
+    else if(n==5)
+    {
+        cout<<"Fri";
+    }
+    else if(n==6)
+    {
+        cout<<"Sat";
+    }
+    else
+    {
+        cout<<"Sun";
+    }
+    return 0;
+}
+
+
+
+
