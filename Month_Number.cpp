99998b05e366ccc56b2dff1542f0b711643ec053
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-06-08 18:14:38 +0530

    Update Month_Number.cpp

--- Month_Number.cpp
+++ Month_Number.cpp
@@ -1,3 +1,43 @@
+//using Switch CASE
+
+#include<iostream>
+
+using namespace std;
+
+void MonthName(int m)
+{
+    
+    switch (m)
+    {
+        case 1: cout<<"jan"; break;
+        case 2: cout<<"feb"; break;
+        case 3: cout<<"mar"; break;
+        case 4: cout<<"apr"; break;
+        case 5: cout<<"may"; break;
+        case 6: cout<<"jun"; break;
+        case 7: cout<<"jul"; break;
+        case 8: cout<<"aug"; break;
+        case 9: cout<<"sept"; break;
+        case 10: cout<<"oct"; break;
+        case 11: cout<<"nov"; break;
+        case 12: cout<<"dec"; break;
+        default:
+        cout<<"Invalid month Number";
+    }
+    
+}
+int main()
+{   
+    int m;
+    cout<<"Enter The Month Number :";
+    cin>>m;
+    MonthName(m);
+}
+
+
+
+//Using ifelse ladder
+/*
 #include<iostream>
 
 using namespace std;
@@ -40,3 +80,6 @@ int main()
     cin>>m;
     MonthName(m);
 }
+*/
+
+
