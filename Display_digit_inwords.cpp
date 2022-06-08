8271991a874eb0ff1497da307f3a6b465e2b78a9
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-06-08 18:27:17 +0530

    Create Display_digit_inwords.cpp

---
+++ Display_digit_inwords.cpp
@@ -0,0 +1,42 @@
+
+    
+#include<iostream>
+
+using namespace std;
+
+void DigitToWord(int d)
+{
+    if(d==1)
+        cout<<"one";
+    else if(d==2)
+        cout<<"two";
+    else if(d==3)
+        cout<<"three";
+    else if(d==4)
+        cout<<"four";
+    else if(d==5)
+        cout<<"five";
+    else if(d==6)
+        cout<<"six";
+    else if(d==7)
+        cout<<"seven";
+    else if(d==8)
+        cout<<"eight";
+    else if(d==9)
+        cout<<"nine";
+    else if(d==0)
+        cout<<"zero";
+    else
+        cout<<"Invalid Number";
+    
+
+    
+    
+}
+int main()
+{   
+    int d;
+    cout<<"Enter The Number :";
+    cin>>d;
+    DigitToWord(d);
+}
