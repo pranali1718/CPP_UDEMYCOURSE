d7ec6928565d90349bd8f132e7a9e2c7e9f5d0bb
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-06-10 11:41:03 +0530

    Update Discount_code.cpp

--- Discount_code.cpp
+++ Discount_code.cpp
@@ -25,3 +25,33 @@ int main()
 	return 0;
 
 }
+
+
+//discounted amount
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float billAmount;
+    float discount=0.0;
+
+    cout<<"Enter Bill Amount:";
+    cin>>billAmount;
+        
+    if(billAmount>=500)
+    {
+    	discount=billAmount*20/100;
+    }
+    else if(billAmount>=100 && billAmount<500)
+    {   
+        discount=billAmount*10/100;
+    }
+    billAmount=billAmount-discount;
+    cout<<"Discount is :"<<discount<<endl;
+    cout<<"Discounted Amount is:"<<billAmount<<endl;
+        
+    return 0;
+        
+}
