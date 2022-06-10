45e0768a1916d7bc70c3feda4c9a4bd366a2de96
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-06-10 14:38:52 +0530

    Create Print_Table.cpp

---
+++ Print_Table.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i ,n;
+    cout<<"Enter Number:";
+    cin>>n;
+    for(i=1;i<=10;i++)
+    {
+        cout<<n*i<<endl;
+        
+    }
+     return 0;
+        
+}
