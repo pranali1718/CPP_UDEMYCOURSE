9730027821bf84f107db30fb20261d5a3670bad9
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-06-14 11:34:02 +0530

    Create Factors.cpp

---
+++ Factors.cpp
@@ -0,0 +1,21 @@
+//find the factors of the given number//
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,i;
+    cout<<"Enter Number :";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            cout<<i<<endl;
+        }
+        
+    }
+
+    return 0;
+}
