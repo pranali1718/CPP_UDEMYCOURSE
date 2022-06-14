df7cab82a89660bc187478a7d75541d6beb9ef31
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-06-14 15:54:34 +0530

    Create Reverse_number.cpp

---
+++ Reverse_number.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+    
+int main()
+{
+	int i,n;
+	cout<<"Enter Number:";
+	cin>>n;
+	while(n>0)
+	{
+	    i=n%10;
+	    n=n/10;
+	    cout<<i;
+	}
+			    
+	return 0;
+	    
+}
