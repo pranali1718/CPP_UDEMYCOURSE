05dbd82f605c6d6940fff574aaa8d3ad2f839dd9
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-06-17 12:24:15 +0530

    Create Sample_code_array.cpp

---
+++ Sample_code_array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    int j;
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+         cout<<"("<<i<<","<<j<<")";
+        }
+    }
+    
+    return 0;
+}
