efccb7ec41a674b86ebbc82065f177e456e0b0f8
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-06-21 17:37:10 +0530

    Update Sample_pointer.cpp

--- Sample_pointer.cpp
+++ Sample_pointer.cpp
@@ -11,3 +11,25 @@ int main()
     cout<<ptr;                      //prints the address of food    
     return 0;
 }
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string food="pizza";
+    cout<<food<<endl;               //prints the food (pizza)
+    cout<< &food<<endl;             //prints the address of food(0x7ffd377e81a0)
+    string *ptr=&food;              //assign pointer to ptr which stores the address of food
+    cout<<ptr<<endl;                //prints the address of food 
+    cout<<*ptr<<endl;               //it prints the original value
+    cout<<&ptr<<endl;               //prints address of pointer
+    return 0;
+}
+
