ef96931635b623a1ce6470219eaf094f13870c7d
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-06-23 10:48:53 +0530

    Create No_argument_return_type.cpp

---
+++ Funtion/No_argument_return_type.cpp
@@ -0,0 +1,17 @@
+//no arguemnt, return type//
+
+#include<iostream>
+using namespace std;
+
+int add();
+
+int main()
+{
+    cout << add();
+}
+
+int add()
+{
+    int x =9,y=7;
+    return x+y;
+}
