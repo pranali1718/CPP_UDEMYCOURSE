530e44c2a74ada48d77b8e64979f8972a41e5ac6
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-06-23 11:23:07 +0530

    Update Factorial.cpp

--- Funtion/Factorial.cpp
+++ Funtion/Factorial.cpp
@@ -25,3 +25,33 @@ int factorial()
     
     return fact;
 }
+
+
+//with noth argument,return type//
+
+//no arguemnt, return type//
+
+#include<iostream>
+using namespace std;
+
+int factorial(int);
+
+int main()
+{
+    int n;
+    cout<<"Enter a Number";
+    cin>>n;
+    cout<<factorial(n);
+}
+
+int factorial(int n)
+{
+    int fact=1;
+    
+    for(int i=1;i<=n;i++)
+    {
+        fact=fact*i;
+    }
+    
+    return fact;
+}
