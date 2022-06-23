86b3e5f0770373b9b24628d3cfd2f5657667f273
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-06-23 11:26:35 +0530

    Update addition_with_argument_with_return_type.cpp

--- Funtion/addition_with_argument_with_return_type.cpp
+++ Funtion/addition_with_argument_with_return_type.cpp
@@ -15,3 +15,24 @@ int add(int x,int y)
 {
     return x+y;
 }
+
+
+//argument ,no return value
+
+#include<iostream>
+using namespace std;
+
+void add(int , int);
+
+int main()
+{
+    int x,y;
+    cout<<"Enter two Number:";
+    cin>>x>>y;
+    add(x,y);
+}
+
+void add(int x,int y)
+{
+    cout<<x+y;
+}
