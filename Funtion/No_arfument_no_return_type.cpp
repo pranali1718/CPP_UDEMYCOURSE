22b86cfb2400427c720580735bdbe8b9ac993828
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-06-23 10:45:54 +0530

    Create No_arfument_no_return_type.cpp

---
+++ Funtion/No_arfument_no_return_type.cpp
@@ -0,0 +1,18 @@
+//no arguemnt no return type//
+
+#include<iostream>
+using namespace std;
+
+void add();
+
+int main()
+{
+    add();
+}
+
+void add()
+{
+    int x =9,y=7;
+    cout<<x+y;
+    
+}
