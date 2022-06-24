ac9c6e02421ca749dbdb7d118aa6904fd68a4c75
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-06-24 14:34:08 +0530

    Create Square.cpp

---
+++ Array_pattern/Square.cpp
@@ -0,0 +1,28 @@
+//Draw Pattern of square
+/*
+
+*****
+*****
+*****
+*****
+*****
+
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<4;j++)
+        {
+            cout<<"*";
+            
+        }
+        cout<<endl;
+    }
+    return 0;
+}
