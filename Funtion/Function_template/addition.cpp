94b8c6893da63d31cd4ccb0abfc042ff9afe65ec
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-06-27 21:58:02 +0530

    Create addition.cpp

---
+++ Funtion/Function_template/addition.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+template< typename T>
+T add(T a,T b)
+{
+    return a+b;
+}
+int main()
+{
+    cout<<add(10,7)<<endl;
+    cout<<add(5.5,6.7);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                                                                                                                                                                                        
