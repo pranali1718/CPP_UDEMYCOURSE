1c10fc696c975b746bae38ea40a314a3679d818d
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-06-27 14:47:19 +0530

    Create Minimum_Numbers.cpp

---
+++ Funtion/Operator_overloading/Minimum_Numbers.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+
+//1 write a Min() function here to Min of 2 integers
+int Min(int x,int y)
+{
+    if(x<y)
+        return x;
+    else 
+        return y;
+}
+//2 write a Min() function here to Min of 3 integers
+int Min(int x,int y,int z)
+{
+    if(x<y && x<z)
+        return x;
+    else if(y<z)
+        return y;
+    else
+        return z;
+}
+
+//3 write a Min() function here to Min of 2 floats
+float Min(float x,float y)
+{
+    if(x<y)
+        return x;
+    else
+        return y;
+}
+
+
+int main()
+{
+    cout<<Min(10,5)<<endl;
+    cout<<Min(12,7,9)<<endl;
+    cout<<Min(18.0f,9.0f);
+    return 0;
+}
