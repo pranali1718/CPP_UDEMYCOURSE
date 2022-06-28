21add6a111d81c368349085aa6599274fe65872e
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-06-28 11:23:22 +0530

    Create marks.cpp

---
+++ Funtion/Ternary_operator/marks.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int marks;
+    cout<<"Enter Marks:";
+    cin>>marks;
+    
+    marks>=35 ? cout<<"Passed" : cout<<"Failed";
+
+    return 0;
+}
+    
