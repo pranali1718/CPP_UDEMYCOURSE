2ad9ec689f1b613346544ac0283739f029d83857
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-07-14 15:16:27 +0530

    Update data_hiding.cpp

--- OOPS/data_hiding.cpp
+++ OOPS/data_hiding.cpp
@@ -58,3 +58,61 @@ int main()
     cout<<"Perimeter Is:"<<r1.perimeter(); 
     return 0;
 }
+
+
+/*--------------------------------------------------------------------------------------------------------------------------------------------*/
+
+#include <iostream>
+using namespace std;
+
+class rectangle
+{
+    int Length=2;                   //passing parameters as private
+    int Breadth=2;
+    
+    public:                         //declaring them as public
+    
+    void Set(int l,int b)
+    {
+        if(l>0)                                 //checking the length is positive
+            Length = l; 
+        else
+        {
+            cout<<"Enter Correct Length"<<endl;
+            exit(0);
+        }
+        
+        if(b>0)                                 //checking the breadth is positive
+            Breadth = b; 
+        else
+        {
+            cout<<"Enter Correct Breadth"<<endl;
+            exit(0);
+        }
+    }
+ 
+    void get()
+    {
+        cin>>Length;
+        cin>>Breadth;
+    }
+   
+    int area()                      //creating fucntion
+    {
+        return Length*Breadth;
+    }   
+    int perimeter()
+    {
+        return 2*(Length+Breadth);
+    }
+};                                  //closing fucntion
+
+int main()
+{
+   
+    rectangle r1;                                     //creating object of class  
+    r1.Set(10,-2);
+    cout<<"Area Is:"<<r1.area()<<endl;                //calling function using object
+    cout<<"Perimeter Is:"<<r1.perimeter(); 
+    return 0;
+}
