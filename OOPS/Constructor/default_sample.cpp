fcee7b6c188b3e0dc2a8ba50b12ed39d8a12267a
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-07-18 17:04:18 +0530

    Create default_sample.cpp

---
+++ OOPS/Constructor/default_sample.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+class emplopyee
+{
+    public:
+    int age;
+    int year;
+   
+    emplopyee()
+    {
+        age=30;
+        year=2002;
+    }
+};
+int main()
+{
+    emplopyee r;
+    cout<<r.age<<endl;
+    cout<<r.year;
+    
+}
