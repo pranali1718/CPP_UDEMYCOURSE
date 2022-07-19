d31a46c1ae77c69b8e37d7bf57b5aaf9964bc361
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-07-19 12:46:31 +0530

    Create sample_code.cpp

---
+++ OOPS/Constructor/copy_constructor/sample_code.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+class sample
+{
+    int a,b;
+    public:
+    sample(int a, int b)
+    {
+        (*this).a=a;
+        (*this).b=b;
+    }
+    sample(sample &old)
+    {
+        a=old.a;
+        b=old.b;
+    }
+    
+    void print()
+    {
+        cout<<"a :"<<a<<" b :"<<b;
+    }
+};
+int main()
+{
+    sample s1(10,20);
+    sample s2(s1);
+    s1.print();
+    cout<<endl;
+    s2.print();
+}
+
