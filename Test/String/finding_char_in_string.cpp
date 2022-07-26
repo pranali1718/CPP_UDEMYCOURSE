b67885fc2ea1b0d6e0cccb4d84d229b9d60c0bc1
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-07-26 17:42:40 +0530

    Create finding_char_in_string.cpp

---
+++ Test/String/finding_char_in_string.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string a;
+    char b;
+    int temp;
+    
+    cout<<"Enter string : ";
+    cin>>a;
+    cout<<"Enter char you want to find : ";
+    cin>>b;
+    cout<<"String : "<<a<<endl;
+    cout<<"char : "<<b<<endl;
+    
+    for(int i=0;i<a.length();i++)
+    {
+        temp = 1;
+        if(a[i]==b)
+        {
+            temp=0;
+            //cout<<"present letter : "<<a[i];
+        }
+        
+    }
+    
+    //cout<<"checking temp value : "<<temp<<endl;
+    
+    if(temp == 0)
+    {
+        cout <<"yes";
+    }
+    else
+    {
+        cout<<"no";
+    }
+   
+    return 0;
+}
