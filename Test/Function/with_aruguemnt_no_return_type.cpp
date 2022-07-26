77a11cd599d0c3c962a091adb6a42a293d452b12
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-07-26 15:07:05 +0530

    Create with_aruguemnt_no_return_type.cpp

---
+++ Test/Function/with_aruguemnt_no_return_type.cpp
@@ -0,0 +1,15 @@
+/*
+with argument no return type
+
+*/
+#include<iostream>
+using namespace std;
+void fun1(int x)
+{
+    cout<<"value of x is :"<<x;
+}
+int main( )
+{
+    fun1(4);
+    return 0;
+}
