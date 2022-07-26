384c12e1a90595933c607ade6641bb1edc2ef84a
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-07-26 15:09:10 +0530

    Create with_arg_with_return_type.cpp

---
+++ Test/Function/with_arg_with_return_type.cpp
@@ -0,0 +1,16 @@
+/*
+with argument with return type
+
+*/
+#include<iostream>
+using namespace std;
+int fun1(int x)
+{
+    cout<<"value of x is :";
+    return x;
+}
+int main( )
+{
+    cout<<fun1(4);
+    return 0;
+}
