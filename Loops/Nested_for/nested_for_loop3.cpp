66ae173e2215255cf04dddb3273ec83401dc2ca7
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-07-27 14:05:44 +0530

    Create nested_for_loop3.cpp

---
+++ Loops/Nested_for/nested_for_loop3.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int i=1;i<3;i++)
+    {
+        cout<<"Value of 1 : "<<i<<endl;
+        cout<<"-------------------------------------------"<<endl;
+        for(int j=1;j<2;j++)
+        {
+            cout<<"Value of   : "<<i<<i<<i<<endl;
+        }
+        cout<<"-------------------------------------------"<<endl;
+        cout<<"-------------------------------------------"<<endl;
+    }
+
+    return 0;
+}
