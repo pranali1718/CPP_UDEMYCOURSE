d7fa5bc4d1df640fe869535b63dac5747cbdd0f5
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-07-28 18:00:34 +0530

    Create Do_while.cpp

---
+++ Test/Loop/Do_while.cpp
@@ -0,0 +1,17 @@
+//odd Number;
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=1;
+    do
+    {
+        cout<<a<<endl;
+        a=a+2;
+    }
+    while(a!=21);
+
+    return 0;
+}
