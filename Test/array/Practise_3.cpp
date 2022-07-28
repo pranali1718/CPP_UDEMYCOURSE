0ecf4490eda76e4d84a9c75aa51f0a636161cf2f
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-07-28 11:45:06 +0530

    Update Practise_3.cpp

--- Test/array/Practise_3.cpp
+++ Test/array/Practise_3.cpp
@@ -3,31 +3,35 @@ Write a program to find the sum and product of all elements of an array.
 
 */
 
-
-#include<iostream>
+#include <iostream>
 using namespace std;
+
+
 int main()
 {
-    int a[5];
-    int sum=0,mul=1;
+    int size;
+    cout<<"Enter Size of array :"<<endl;
+    cin>>size;
+    int a[size];
+    int sum=0;
+    int mul=1;
+    int average=0;
     
-    for(int i=0;i<5;i++)
+    cout<<"Enter array Elements :";
+    for(int i=0;i<size;i++)
     {
         cin>>a[i];
     }
-    cout<<"array  is :";
-    for(int i=0;i<5;i++)
+    cout<<"Array Elements are : ";
+    for(int i=0;i<size;i++)
     {
         cout<<a[i]<<" ";
-    }
-    cout<<endl;
-    
-    for(int i=0;i<5;i++)
-    {
         sum=sum+a[i];
         mul=mul*a[i];
+        average =sum/size;
     }
-    cout<<"Addition of all array element is : "<<sum<<endl;
-    cout<<"Multiplcation of all array element is : "<<mul<<endl;
-    return 0;
+    cout<<"\nSum : "<<sum<<endl;
+    cout<<"Mul : "<<mul<<endl;
+    cout<<"avg : "<<average;
+    
 }
