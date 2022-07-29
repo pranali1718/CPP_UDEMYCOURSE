f12ba270491e892cefeee7c50a7e43a3af452622
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-07-30 00:53:40 +0530

    Create Pattern6.cpp

---
+++ Test/array/2d_array/Pattern/Pattern6.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int n=1;
+int main()
+{
+    for(int i=0;i<=5;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            if(n<=25)
+            cout<<n<<"  ";
+            n++;
+        }
+         cout<<endl;
+    }
+    return 0;
+}
