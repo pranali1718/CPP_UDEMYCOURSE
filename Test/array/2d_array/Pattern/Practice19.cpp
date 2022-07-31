f1390b10bec754389a8084c497c2da39b053eae5
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-07-31 16:27:29 +0530

    Create Practice19.cpp

---
+++ Test/array/2d_array/Pattern/Practice19.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            cout<<((i+j)%2)<<" "; 
+        }
+        cout<<endl;
+    }               
+    return 0;
+}
