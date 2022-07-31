8c11d91b67834e1f51046eebba700490530c42fe
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-07-31 17:33:33 +0530

    Create Practice25.cpp

---
+++ Test/array/2d_array/Pattern/Practice25.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            cout<<(j%2)<<" ";
+        }
+        
+        cout<<endl;
+    }               
+    return 0;
+}
