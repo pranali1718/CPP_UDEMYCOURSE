0932491c33d9d09e6914f796f47c2457ae30de43
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-08-02 11:23:01 +0530

    Create add.cpp

---
+++ Test/Function/with_argument_with_return_type/add.cpp
@@ -0,0 +1,22 @@
+/*
+1.with argument ,with return type
+
+Write a program to print the sum of two numbers entered by user by defining your own function.
+*/
+
+#include<iostream>
+using namespace std;
+
+int fun(int x,int y)
+{
+    return x+y;
+}
+
+int main()
+{
+    int a,b;
+    cout<<"Enter two no :";
+    cin>>a>>b;
+    cout<<fun(a,b);
+}
+
