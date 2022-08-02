f906ce54a185df6596aeba7b0b1421fe4571ee92
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-08-02 12:06:31 +0530

    Create Odd_Even.cpp

---
+++ Test/Function/with_argument_with_return_type/Odd_Even.cpp
@@ -0,0 +1,25 @@
+
+#include<iostream>
+using namespace std;
+
+int odd_even(int x)
+{
+    if(x%2==0)
+    {
+        cout<<"even";
+    }
+    else
+    {
+        cout<<"odd";
+    }
+    return 0;
+}
+int main()
+{
+    int a;
+    cout<<"Enter No :";
+    cin>>a;
+    odd_even(a);
+    return 0;   
+}
+
