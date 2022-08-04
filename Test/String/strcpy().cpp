441f208f272628133b23e7377b486fa66db7aa12
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-08-04 12:04:38 +0530

    Update strcpy().cpp

--- Test/String/strcpy().cpp
+++ Test/String/strcpy().cpp
@@ -13,3 +13,22 @@ int main()
     cout<<"string 2:"<<b;
     return 0;
 }
+
+
+/*ommiting namespace ,using std::*/
+
+#include <iostream>
+#include <cstring>
+//using namespace std;
+
+int main()
+{
+    char a[]="abc";
+    char b[]="gfo";
+    
+    strcpy(a,b);
+    
+    std::cout<<"string 1:"<<a<<std::endl;
+    std::cout<<"string 2:"<<b;
+    return 0;
+}
