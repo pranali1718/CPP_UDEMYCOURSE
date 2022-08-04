9f9e96c8899d88832dec99e533588d87cb87a27d
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-08-04 10:42:33 +0530

    Create find_length.cpp

---
+++ Test/String/find_length.cpp
@@ -0,0 +1,14 @@
+/*Write a program to find the length of the string "refrigerator".*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string a= "refrigerator";
+    int b=a.length();
+    cout<<b;
+    
+    return 0;
+}
