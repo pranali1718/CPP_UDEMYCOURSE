5ae6bdf44372d08a3eaa636053b5ee74d808ce96
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-08-09 12:57:38 +0530

    Create table print.cpp

---
+++ Test/Loop/for/table print.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+   int a;
+   cout<<"Enter no want to table : ";
+   cin>>a;
+   for(int i=1;i<11;i++)
+   {
+       cout << a<<" x "<< i << " = "<<a*i<<"\n" ;
+   }
+   
+}
