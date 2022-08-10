c2cd93d2837af3c7406c9d4bd68d3da84091aa6b
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-08-10 10:26:45 +0530

    Create Append.cpp

---
+++ Test/String/STRING/Append.cpp
@@ -0,0 +1,17 @@
+/*
+In this code I used append()
+    by this i can assign new string to the previous string
+    
+    SYNTAX:
+        str_name.append("append_line")
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string a="Good Morning";
+    a.append(" Good Evening");
+    cout<<"\n string after appending :"<<a;
+    return 0;
+}
