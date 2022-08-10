fba254a6c72cf39fdc7f18dbd3a5c567779a5f1a
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-08-10 10:52:30 +0530

    Create Find letter using index at().cpp

---
+++ Test/String/STRING/Find letter using index at().cpp
@@ -0,0 +1,17 @@
+/*
+In this code I used .at(NO)
+    by this i can assign new string to the previous string
+    
+    SYNTAX:
+        str_name.at(Index)
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string a="Good Morning";
+    a.at(3);
+    cout<<"\nString after appending :"<<a.at(3);
+    return 0;
+}
