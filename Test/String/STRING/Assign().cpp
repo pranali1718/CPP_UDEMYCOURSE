4bf7aea0d93fba3182033a7a84981a64846eb11f
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-08-10 10:25:04 +0530

    Create Assign().cpp

---
+++ Test/String/STRING/Assign().cpp
@@ -0,0 +1,14 @@
+/*
+In this code I used assign()
+    by this i can assign new string to the previous string
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string a="Good Morning";
+    a.assign("Good Evening");
+    cout<<"\n string after assign :"<<a;
+    return 0;
+}
