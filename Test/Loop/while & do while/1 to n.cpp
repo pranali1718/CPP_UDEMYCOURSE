7a007c82bcfcd749811eaddf1d92cba4909bee5a
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-08-10 14:39:45 +0530

    Create 1 to n.cpp

---
+++ Test/Loop/while & do while/1 to n.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+/*int main()
+{
+    int n=1;
+    while(n<=10)
+    {
+        cout<<n<<endl;
+        n++;
+    }
+        return 0;
+}*/
+
+int main()
+{
+    int n=1;
+    do
+    {
+        cout<<n<<endl;
+        n++;
+    }
+    while(n<=10);
+    
+}
