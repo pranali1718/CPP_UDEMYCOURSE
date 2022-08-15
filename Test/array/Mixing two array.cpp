64a63cf90cee8eb15f41c74e3b19068ff548cb8f
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-08-15 14:10:48 +0530

    Create Mixing two array.cpp

---
+++ Test/array/Mixing two array.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string a="PRANALI";
+    string b="MAHALE";
+    
+    for(int i=0;i<a.length();i++)
+    {
+        for(int j=0;j<1;j++)
+        {
+            cout<<a[i]<<" "<<b[i]<<" ";
+        }
+    }
+    return 0;
+    
+}
