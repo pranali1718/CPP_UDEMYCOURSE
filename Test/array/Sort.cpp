18ace141cd47bbbadf4a56fe1289052fa2b325f7
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-08-18 12:43:59 +0530

    Update Sort.cpp

--- Test/array/Sort.cpp
+++ Test/array/Sort.cpp
@@ -37,3 +37,49 @@ int main()
 	return 0;
 
 }
+
+
+---------------------------------------------------------------------------------------------------------------------------------
+	
+	#include<iostream>
+using namespace std;
+
+void sorting(int arr[],int size)
+{
+int temp;
+
+for(int i=0;i<5;i++)
+{
+	for(int j=0;j<5;j++)
+	{
+		
+		    if(arr[j]>=arr[j+1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            } 
+		
+	}
+}
+cout<<"Sorted Array : ";
+for(int i=0;i<5;i++)
+{
+    cout<<arr[i]<<" ";
+}
+}
+int main()
+{
+	int a[5];
+	int temp;
+	cout<<"Enter 5 Elements : ";
+	for(int i=0;i<5;i++)
+	{
+		cin>>a[i];
+	}
+	
+	sorting(a,5);
+	return 0;
+
+}
+
