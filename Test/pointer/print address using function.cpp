650a038f70533876007a2cf9ebb2664ccca96b12
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-08-19 11:30:42 +0530

    Update print address using function.cpp

--- Test/pointer/print address using function.cpp
+++ Test/pointer/print address using function.cpp
@@ -18,3 +18,25 @@ int main()
     fun(&a);
     return 0;
 }
+-----------------------------------------------------------------------------------------------------------------------------
+    
+    /*Write a program to print the value of the address of the pointer to a variable whose value is input from user.*/
+
+#include <iostream>
+using namespace std;
+            
+void fun()                                                  //function
+{
+    int a,*p; 
+    cout<<"Enter Number :";
+    cin>>a;
+    p=&a;
+    cout<<"Address of "<< a<<" is : "<<p<<endl;             //print address of int
+    cout<<"value Enter By You Is : "<<*p;                 //dereferencing 
+}
+
+int main()
+{
+    fun();
+    return 0;
+}
