de97a000d025fa9b20ec14233fd8d4ab65988afc
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-08-22 23:28:27 +0530

    Update Arithmatic Pointer.cpp

--- Test/pointer/Pointer & Array/Arithmatic Pointer.cpp
+++ Test/pointer/Pointer & Array/Arithmatic Pointer.cpp
@@ -4,6 +4,42 @@ using namespace std;
 int main()
 {
     int var[3]={100,200,300};
+    int *ptr;
+    
+    cout<<"Array Is : ";
+    for(int i=0;i<3;i++)                        //printing Array
+    {
+        cout<<var[i]<<" ";
+    }
+    cout<<endl;
+    
+    for(int i=0;i<3;i++)                        //assignning the add to pointer
+    {
+        ptr=var;
+    }
+    cout<<endl;
+    
+    for(int i=0;i<3;i++)                        //print the values
+    {       
+        cout<<*ptr<<" ";
+        ptr++;                                  //arithatic opearator for pointer array
+    }
+    cout<<endl;
+    
+    for(int i=0;i<3;i++)                        //print the adddress of values
+    {       
+        cout<<ptr<<" ";
+        ptr++;
+    }
+}
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int var[3]={100,200,300};
     int *ptr[3];
     cout<<"Array Is : ";
     for(int i=0;i<3;i++)
