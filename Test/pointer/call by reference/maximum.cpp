dbde8b5dbab285d8cec9e60cbfceb1e3eded9779
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-08-22 18:23:11 +0530

    Update maximum.cpp

--- Test/pointer/call by reference/maximum.cpp
+++ Test/pointer/call by reference/maximum.cpp
@@ -1,1 +1,24 @@
 
+#include <iostream>
+using namespace std;
+
+void maximum(int &x,int &y)                    
+{
+   if(x>y)
+   {
+       cout<<"maximum is : "<<x;
+   }
+   else
+   {
+       cout<<"Maximum is : "<<y;
+   }
+}          
+int main()
+{
+    int a,b;
+    cout<<"Enter Two Number : ";
+    cin>>a>>b;
+    maximum(a,b);                             //passing the varibles
+    
+    return 0;
+}
