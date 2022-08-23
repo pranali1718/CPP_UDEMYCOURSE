f6d09c53a572990bdc0538de88b9a2348b8e0399
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-08-23 11:50:32 +0530

    Create 1.cpp

---
+++ Test/pointer/Arithematic_Pointer/1.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int var[3]={100,200,300};
+    int *ptr;
+    
+    cout<<"Array Is : ";
+    for(int i=0;i<3;i++)                        //printing Array
+    {
+        cout<<var[i]<<" ";
+    }
+    cout<<endl;
+    
+    for(int i=0;i<3;i++)                        //assignning the add to pointer
+    {
+        ptr=var;
+    }
+    cout<<endl;
+    
+    for(int i=0;i<3;i++)                        //print the values
+    {       
+        cout<<*ptr<<" ";
+        ptr++;                                  //arithatic opearator for pointer array
+    }
+    cout<<endl;
+    
+    for(int i=0;i<3;i++)                        //print the adddress of values
+    {       
+        cout<<ptr<<" ";
+        ptr++;
+    }
+}
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int var[3]={100,200,300};
+    int *ptr[3];
+    cout<<"Array Is : ";
+    for(int i=0;i<3;i++)
+    {
+        cout<<var[i]<<" ";
+    }
+    cout<<endl;
+    for(int i=0;i<3;i++)
+    {
+        ptr[i]=&var[i];
+    }
+    cout<<endl;
+    for(int i=0;i<3;i++)
+    {
+        cout<<*ptr[i]<<" ";
+    }
+}
+------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
+    
+    #include <iostream>
+using namespace std;
+
+int main()
+{
+    int var[3]={100,200,300};
+    int *ptr;
+    
+    cout<<"Array Is : ";
+    for(int i=2;i>=0;i--)                        //printing Array
+    {
+        cout<<var[i]<<" ";
+    }
+    cout<<endl;
+    
+    for(int i=2;i>=0;i--)                        //assignning the add to pointer
+    {
+        ptr=&var[2];
+    }
+    cout<<endl;
+    
+    for(int i=2;i>=0;i--)                        //print the values
+    {       
+        cout<<*ptr<<" ";
+        ptr--;                                  //arithatic opearator for pointer array
+    }
+    cout<<endl;
+    
+    for(int i=2;i>=0;i--)                        //print the adddress of values
+    {       
+        cout<<ptr<<" ";
+        ptr--;
+    }
+}
+
+
+
