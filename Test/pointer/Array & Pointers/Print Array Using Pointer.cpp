752d7a259a258a1f91ea7dc2dcf77ea202f4217a
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-08-23 18:01:43 +0530

    Update Print Array Using Pointer.cpp

--- Test/pointer/Array & Pointers/Print Array Using Pointer.cpp
+++ Test/pointer/Array & Pointers/Print Array Using Pointer.cpp
@@ -27,3 +27,28 @@ int main()
     }
     return 0;
 }
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a[5],*ptr;                                          //int array & int pointer
+    ptr=a;                                                  //assigning address to pointer
+    
+    cout<<"Enter 5 elemets :";                              
+    for(int i=0;i<5;i++)
+    {
+        cin>>ptr[i];                                        //storing array elemets in pointer
+        ptr[i]=a[i];                                        //assigning array elemets to pointer address    
+    }
+    cout<<endl;
+    for(int i=0;i<5;i++)                                    
+    {
+        cout<<"Address of "<<a[i]<<" is "<<&ptr[i]<<" "<<endl;                //printing array elements & address of that elements
+    }
+    return 0;
+}
+
+
