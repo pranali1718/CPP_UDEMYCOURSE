c9a9656a4e0fd77146ce4069f764ca93a08c67f5
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-08-24 14:14:04 +0530

    Create Sorting.cpp

---
+++ Test/pointer/Array & Pointers/Dynamic Allocation of Array/Sorting.cpp
@@ -0,0 +1,45 @@
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int size,temp;
+    
+    cout<<"Enter size :";
+    cin>>size;
+    
+    char *cp=new char[size];
+    
+    cout<<"Enter Charaters :";
+    for(int i=0;i<size;i++)
+    {
+        cin>>cp[i];
+    }
+    
+    cout<<"Entered charaters : ";
+    for(int i=0;i<size;i++)
+    {
+        cout<<*(cp+i)<<" ";
+    }
+    
+    cout<<"\nSorted charaters : ";
+    for(int i=0 ; i<size ;i++ )
+    {
+        for(int j=0 ; j<size-1 ; j++)
+        {
+            if(cp[j]>=cp[j+1])
+            {
+                temp = cp[j];
+                cp[j] = cp[j+1];
+                cp[j+1] = temp;
+            } 
+        }
+    }
+    for(int i=0;i<size;i++)
+    {
+        cout<<cp[i]<<" ";
+    }
+    delete(cp);
+    return 0;
+}
