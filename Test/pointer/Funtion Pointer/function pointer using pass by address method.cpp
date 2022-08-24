198d498a444d198bc588c2ae67e735c00915c4df
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-08-24 15:46:12 +0530

    Create function pointer using pass by address method.cpp

---
+++ Test/pointer/Funtion Pointer/function pointer using pass by address method.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int fun(int *a,int *b)
+{
+    return *a+*b;
+}
+
+int main()
+{
+    int a,b;
+    cout<<"Enter Values :";
+    cin>>a>>b;
+    int (*funcptr)(int * ,int *);
+    funcptr=fun;
+    cout<<funcptr(&a,&b);
+    return 0;
+}
+
+
