7c1459e5f5d64a83ded70ca5d98152677f455b6f
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-08-24 11:11:51 +0530

    Update sample.cpp

--- Test/pointer/Dynamic Allocation/sample.cpp
+++ Test/pointer/Dynamic Allocation/sample.cpp
@@ -3,14 +3,17 @@ using namespace std;
 
 int main()
 {
-    int a=5,*p;
+    int *p;
     
     p=new int;
     
     *p=89;
     cout<<" *p is: "<<*p<<endl;
-    cout<<" address of *p is: "<<p;
+    cout<<" address of *p is: "<<p<<endl;
     
+    delete(p);
+    
+    cout<<" after deleting it becomes :"<<*p;
 
     return 0;
 }
