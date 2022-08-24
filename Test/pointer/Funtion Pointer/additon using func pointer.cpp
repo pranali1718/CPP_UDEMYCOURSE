4ebf48a238b79defd4d4e428ea75d02429521855
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-08-24 15:26:37 +0530

    Update additon using func pointer.cpp

--- Test/pointer/Funtion Pointer/additon using func pointer.cpp
+++ Test/pointer/Funtion Pointer/additon using func pointer.cpp
@@ -12,3 +12,20 @@ int main()
     ptr = &fun;
     ptr();
 }
+---------------------------------------------------------------------------------------------------------------------------------------
+    #include<iostream>
+using namespace std;
+
+void add(int a,int b)
+{
+    cout<<"add is :"<<a+b;
+}
+
+
+int main()
+{
+    void(*funcptr)(int ,int );
+    funcptr=add;                                //function pointing toward the func pointer 
+    funcptr(5,5);                               //passing arguemnts to function by function pointer
+    return 0;
+}
