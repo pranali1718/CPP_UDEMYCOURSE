6967425aa48dfe5739e2f2a7fcca2f9c65791a8e
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-08-25 17:25:42 +0530

    Update character.cpp

--- Test/pointer/character.cpp
+++ Test/pointer/character.cpp
@@ -15,3 +15,22 @@ int main()
     
     return 0;
 }
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
+#include<iostream>
+#include<string>
+using namespace std;
+
+void fun(int* p,char x[] )
+{
+    cout<<"Int Pointer: "<<*p<<" \nchar Pointer: "<<x;
+}
+
+int main()
+{
+    int a=7;
+    char l[]="Pranali";
+    fun(&a,l);
+    
+    return 0;
+}
