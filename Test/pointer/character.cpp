95cd1c6285304a54e1cf29395c27325699fe93f3
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-08-25 17:23:57 +0530

    Create character.cpp

---
+++ Test/pointer/character.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+void fun(int* p,char *x )
+{
+    cout<<"Int Pointer: "<<*p<<" \nchar Pointer: "<<x;
+}
+
+int main()
+{
+    int a=7;
+    char *l="Pranali";
+    fun(&a,l);
+    
+    return 0;
+}
