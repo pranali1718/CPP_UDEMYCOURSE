c3f99eee90118ed1d5282fc40b51ab41d647066d
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-08-29 22:42:30 +0530

    Create Basic.cpp

---
+++ Test/Lab Assignment/Basic.cpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+Write a program to display output using a single cout  statement.
+Subject Marks
+---------------------------
+Mathematics 90
+Computer 77
+Chemistry 69
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Mathematics   90"<<endl;
+    cout<<"Computer  77"<<endl;
+    cout<<"Chemistry  69";
+
+    return 0;
+}
