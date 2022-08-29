442483cbe5c045f2ad0c7fb95d40c31d0f8693da
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-08-29 11:52:14 +0530

    Create 1.cpp

---
+++ Test/Operator Overloading/Unary Overloading/-Overloading/1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+
+
+class Base
+{
+    int x,y,z;
+    public:
+    
+    void setdata(int a,int b,int c)
+    {
+        x=a;
+        y=b;
+        z=c;
+    }
+    
+    void dispaly()
+    {
+        cout<<x<<" "<<y<<" "<<z<<endl;
+    }
+    
+    void operator -()
+    {
+        x=-x;
+        y=-y;
+        z=-z;
+    }
+};
+int main()
+{
+    Base b;
+    b.setdata(4,-9,-8);
+    b.dispaly();
+    
+    -b;
+    b.dispaly();
+    
+    return 0;
+}
