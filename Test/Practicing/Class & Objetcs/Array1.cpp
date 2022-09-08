3a4373e5a169047af2941a11fa8f4d10a9e155cd
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-09-08 12:30:24 +0530

    Create Array1.cpp

---
+++ Test/Practicing/Class & Objetcs/Array1.cpp
@@ -0,0 +1,42 @@
+/*******************************************************************************************
+                                array in class
+********************************************************************************************/
+#include <iostream>
+using namespace std;
+
+#define size 10
+
+class Sample
+{
+     int a[size];
+    public:
+   
+    void set_val()
+    {
+        cout<<"size : "<<size<<endl;
+        cout<<"Enter Array Elements : ";
+        for(int i=0;i<size;i++)
+        {
+            cin>>a[i];
+        }
+       
+    }
+    
+    void get_val()
+    {
+        cout<<"Array Elements are : ";
+        for(int i=0;i<size;i++)
+        {
+            cout<<a[i]<<" ";
+        }
+    }
+   
+};
+
+int main()
+{
+    Sample s;
+    s.set_val();
+    s.get_val();
+    return 0;
+}
