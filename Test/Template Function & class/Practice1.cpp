f7a10ca1bbab52a7d1caf4d618267ccbf79cb0ad
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-09-16 12:35:59 +0530

    Update Practice1.cpp

--- Test/Template Function & class/Practice1.cpp
+++ Test/Template Function & class/Practice1.cpp
@@ -19,3 +19,30 @@ int main()
     cout<<"Addition is : "<<add(a,b);
     return 0;
 }
+
+
+---------------------------------------------------------------------
+  /******************************************************************************
+                        Template Function
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+template <typename T>
+T add(T a, T b)
+{
+    T result = a+b;
+    return result;
+}
+
+int main()
+{
+    int x;
+    float y ;
+    x = add(3,2);
+    y = add(5.5,4.4);
+    cout<<"Addition is :"<<x<<endl;
+    cout<<"Addition is :"<<y<<endl;
+    return 0;
+}
