c759a49ac44b0c36a43743b64eedf8c9b371cd39
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-09-17 12:34:02 +0530

    creating a constant integer

---
+++ Test/Constant Qualifier/Constant Integer.cpp
@@ -0,0 +1,16 @@
+/******************************************************************************
+ * Constant Qualifier
+ * use "const" keyword 
+ * the value throught the program cant get changed
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int a=9;
+    cout<<"Value of a is : "<<a;
+    return 0;
+}
