c7fce53917107d8472fef987addcce6f9f590009
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-09-17 12:45:49 +0530

    Create Constant Pointer.cpp

---
+++ Test/Constant Qualifier/Constant Pointer.cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+ * Constant Qualifier-(constant pointer)
+ * use "const" keyword 
+ * the value throught the program cant get changed
+ * here data is constant but we can assign any new value to the same pointer 
+                        or
+ * we can assign the another value to the same pointer address
+ 
+ * Syntax:
+        const int *ptr;
+ 
+ *******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int a = 9;                        //constant value assign to a
+    const int b = 8;                        //constant value assign to b
+    const int *ptr = &a;
+   
+    ptr=&b;                                 
+    cout<<"Value of a is : "<<a<<endl;
+    cout<<"Value of b is : "<<b<<endl;
+    cout<<"address of a is : "<<&a<<endl;
+    cout<<"address of b is : "<<ptr<<endl;
+    
+    return 0;
+}
