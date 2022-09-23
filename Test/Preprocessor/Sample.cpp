b0f85866a9f08075ce65bab064c652738d6ca3ca
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-09-23 10:34:38 +0530

    Create Sample.cpp

---
+++ Test/Preprocessor/Sample.cpp
@@ -0,0 +1,15 @@
+/******************************************************************************
+ * Preprocessor
+ * 1.	This are nothing but instruction to compiler
+ * 2.   using this we can also write functions.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+#define a 20;
+int main()
+{
+    cout<<"A : "<<a;
+
+    return 0;
+}
