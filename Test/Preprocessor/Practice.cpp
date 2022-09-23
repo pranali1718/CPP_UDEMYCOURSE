a622e18e2d72ae3bd6a6d72cc134e25ff666b0bd
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-09-23 10:33:23 +0530

    Create Practice.cpp

---
+++ Test/Preprocessor/Practice.cpp
@@ -0,0 +1,19 @@
+/******************************************************************************
+ * Preprocessor
+ * 1.	This are nothing but instruction to compiler
+ * 2.   using this we can also write functions.
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+#define a 9
+int b =3;
+int main()
+{
+    int p=4;
+    cout<<"a : "<<a;
+    cout<<"\nb : "<<b;
+
+    return 0;
+}
