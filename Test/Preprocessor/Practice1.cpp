ef710193fc69a4dde4d3a66317214a6c6936451d
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-09-23 12:31:16 +0530

    string

--- Test/Preprocessor/Practice1.cpp
+++ Test/Preprocessor/Practice1.cpp
@@ -1,15 +1,17 @@
 /******************************************************************************
  * Preprocessor
+ * #x is mean to be "  ", it dirreclt converted into string 
+ *
 *******************************************************************************/
 
 #include <iostream>
 using namespace std;
 
 #define SQR(x) (x*x)
-
+#define MSG(x) #x
 int main()
 {
-    cout<<SQR(5);
-
+    cout<<SQR(5)<<endl;
+    cout<<MSG(Pranali mahale);
     return 0;
 }
