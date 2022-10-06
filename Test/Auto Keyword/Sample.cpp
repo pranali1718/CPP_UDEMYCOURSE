8e7ad51407c09b7341d88cb17cf02ddf7e9d6ffe
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-10-06 14:49:37 +0530

    Create Sample.cpp

---
+++ Test/Auto Keyword/Sample.cpp
@@ -0,0 +1,25 @@
+/******************************************************************************
+ * Auto Keyword
+  
+ * automatically compiler decides which type of value it is 
+ * 
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+
+void add()
+{
+    float x;
+    int y;
+    cout<<"Enter The value x & y : ";
+    cin>>x>>y;
+    auto z=x+y;
+    cout<<"\nadd : "<<z;
+}
+int main()
+{   
+    add();
+    return 0;
+}
