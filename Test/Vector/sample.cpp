bd8c9b66e541ba920886fc9b73e9a7d0c33658ef
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-10-18 22:23:58 +0530

    Create sample.cpp

---
+++ Test/Vector/sample.cpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+ * Vector
+ * no need to specify size
+ * we can add element as we want.
+ * size automatically increase as we adding the elements 
+ * Syntax 
+ *      vector <data_type> vector_name;  
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    vector <int> vec1;              //declaring vector
+
+    return 0;
+}
