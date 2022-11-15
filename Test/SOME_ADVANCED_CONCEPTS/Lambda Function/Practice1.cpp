11a4cd01b4829a3f361c98db0995b3b07d83b572
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-11-15 15:23:10 +0530

    using return type

---
+++ Test/SOME_ADVANCED_CONCEPTS/Lambda Function/Practice1.cpp
@@ -0,0 +1,32 @@
+/******************************************************************************
+ *lambda Function
+ * [ ] = known as capturelist or lambda introduces
+ * ( ) = known as parameter list
+ * Syntax :
+        [capture_list](parameter_list)->return_type{body}(f_call);
+ * ---------------------------------------------------------------------------
+ * 
+ * code : to see how to use return type
+ *      Syntax:
+            int s = [](int x,int y)->return_type{body}(f_call);     
+ * 
+ * 
+ *******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    []()                                    //function for printing
+    {
+        cout<<"Hello World!!"<<endl;
+    }();
+        
+    cout<<"SUM IS : "<<([](int x,int y)                         //paramterised fucntion 
+    {
+        return x+y;
+    }(10,5));
+    
+    return 0;
+}
